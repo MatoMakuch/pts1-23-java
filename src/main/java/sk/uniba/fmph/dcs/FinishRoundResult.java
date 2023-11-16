748130eb88dea2904c9f45a8775679c5089801f8
MatLukac <mat.lukac13@gmail.com>
2023-11-16 16:32:01 +0100

    Create FinishRoundResult.java

---
+++ src/main/java/sk/uniba/fmph/dcs/FinishRoundResult.java
@@ -0,0 +1,13 @@
+package sk.uniba.fmph.dcs;
+
+enum FinishRoundResult {
+    NORMAL,
+    GAME_FINISHED;
+
+    @Override
+    public String toString() {
+        switch(this) {
+            return super.toString();
+        }
+    }
+}
